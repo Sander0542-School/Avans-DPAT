1695a5e49b15db2acc6de079231b418cb98b9912
Sander Jochems <sanderjochems@hotmail.nl>
2022-05-16 13:43:26 +0200

    Create ISudokuFactory.cs

---
+++ src/Avans.DPAT.Sudoku.Persistence/Factories/ISudokuFactory.cs
@@ -0,0 +1,8 @@
+﻿using File=Avans.DPAT.Sudoku.Persistence.Models.File;
+
+namespace Avans.DPAT.Sudoku.Persistence.Factories;
+
+public interface ISudokuFactory
+{
+    public Game.Sudoku CreateSudoku(File file);
+}
