6529c67088905a34757b270091c19cdfa3de2962
Sander Jochems <sanderjochems@hotmail.nl>
2022-05-30 13:00:50 +0200

    Create FileExtensions.cs

---
+++ src/Avans.DPAT.Sudoku.Persistence/Extensions/FileExtensions.cs
@@ -0,0 +1,11 @@
+﻿using File=Avans.DPAT.Sudoku.Persistence.Models.File;
+
+namespace Avans.DPAT.Sudoku.Persistence.Extensions;
+
+public static class FileExtensions
+{
+    public static string[] Lines(this File file)
+    {
+        return file.Contents.Split('\n');
+    }
+}
