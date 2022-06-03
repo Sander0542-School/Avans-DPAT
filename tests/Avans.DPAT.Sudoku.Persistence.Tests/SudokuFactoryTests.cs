8bd165a4cf13ef9cc7ddbc497024219de785aa29
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-03 17:57:50 +0200

    Update SudokuFactoryTests.cs

--- tests/Avans.DPAT.Sudoku.Persistence.Tests/SudokuFactoryTests.cs
+++ tests/Avans.DPAT.Sudoku.Persistence.Tests/SudokuFactoryTests.cs
@@ -1,5 +1,7 @@
-﻿using Avans.DPAT.Sudoku.Persistence.Factories;
+﻿using System;
+using Avans.DPAT.Sudoku.Persistence.Factories;
 using Avans.DPAT.Sudoku.Persistence.Loaders;
+using Avans.DPAT.Sudoku.Persistence.Models;
 using Xunit;
 
 namespace Avans.DPAT.Sudoku.Persistence.Tests;
@@ -48,5 +50,52 @@ public class SudokuFactoryTests
         var sudoku = factory.CreateSudoku(file);
 
         Assert.Equal(441, sudoku.Cells.Length);
+    }
+
+    [Fact]
+    public void Test_Jigsaw_Loads()
+    {
+        var file = new FileSystemFileLoader().Load("Puzzles/puzzle.jigsaw");
+
+        var factory = new JigsawSudokuFactory();
+        var sudoku = factory.CreateSudoku(file);
+
+        Assert.Equal(81, sudoku.Cells.Length);
+    }
+
+    [Theory]
+    [InlineData("Puzzles/puzzle.4x4")]
+    [InlineData("Puzzles/puzzle.6x6")]
+    [InlineData("Puzzles/puzzle.9x9")]
+    [InlineData("Puzzles/puzzle.samurai")]
+    [InlineData("Puzzles/puzzle.jigsaw")]
+    public void Test_Factory_Loads(string filePath)
+    {
+        var file = new FileSystemFileLoader().Load(filePath);
+
+        var factory = new SudokuFactory();
+        Assert.True(factory.Supports(file));
+
+        var sudoku = factory.CreateSudoku(file);
+
+        Assert.NotNull(sudoku);
+    }
+
+    [Fact]
+    public void Test_Factory_DoesntSupport_InvalidExtension()
+    {
+        var file = new File("Puzzles/puzzle.invalid", "01024274300");
+
+        var factory = new SudokuFactory();
+        Assert.False(factory.Supports(file));
+    }
+
+    [Fact]
+    public void Test_Factory_ThrowException_InvalidExtension()
+    {
+        var file = new File("Puzzles/puzzle.invalid", "01024274300");
+
+        var factory = new SudokuFactory();
+        Assert.Throws<NotImplementedException>(() => factory.CreateSudoku(file));
     }
 }
