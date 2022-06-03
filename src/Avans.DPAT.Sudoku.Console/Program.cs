a0cef92b6d330f47397e9d2bcc417cb484fadbc7
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-03 09:35:54 +0200

    Upload game files

--- src/Avans.DPAT.Sudoku.Console/Program.cs
+++ src/Avans.DPAT.Sudoku.Console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Avans.DPAT.Sudoku.Game.Grid;
+using Avans.DPAT.Sudoku.Persistence.Factories;
+using Avans.DPAT.Sudoku.Persistence.Loaders;
+
+var fileLoader = new FileSystemFileLoader();
+var file = fileLoader.Load("Puzzles/puzzle.9x9");
+
+var factory = new SudokuFactory();
+var sudoku = factory.CreateSudoku(file);
+
+var cells = new GridCell[sudoku.Numbers, sudoku.Numbers];
+sudoku.Grid.Flatten(cells);
+
+for (var x = 0; x < cells.GetLength(0); x++)
+{
+    for (var y = 0; y < cells.GetLength(1); y++)
+    {
+        var value = cells[x, y].Value;
+        Console.Write(value.HasValue ? value.Value.ToString() : " ");
+    }
+    Console.WriteLine();
+}
