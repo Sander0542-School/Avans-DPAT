6745b5c5c89129d23581591016a5ad43af81dd84
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-03 15:06:21 +0200

    Create sample test

---
+++ tests/Avans.DPAT.Sudoku.Persistence.Tests/SudokuFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Avans.DPAT.Sudoku.Persistence.Factories;
+using Avans.DPAT.Sudoku.Persistence.Loaders;
+using Xunit;
+
+namespace Avans.DPAT.Sudoku.Persistence.Tests;
+
+public class SudokuFactoryTests
+{
+    [Fact]
+    public void Test_Factory_Works()
+    {
+        var file = new FileSystemFileLoader().Load("Puzzles/puzzle.9x9");
+        
+        var factory = new SudokuFactory();
+        var sudoku = factory.CreateSudoku(file);
+        
+        Assert.NotNull(sudoku);
+    }
+}
