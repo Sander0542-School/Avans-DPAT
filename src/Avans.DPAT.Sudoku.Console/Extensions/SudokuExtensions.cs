ccbd409938a553176724a1c21fc08c88d1949f0f
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-06 12:51:37 +0200

    Create SudokuExtensions.cs

---
+++ src/Avans.DPAT.Sudoku.Console/Extensions/SudokuExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Avans.DPAT.Sudoku.Console.Extensions;
+
+public static class SudokuExtensions
+{
+    public static int Height(this Game.Sudoku sudoku)
+    {
+        return sudoku.Cells.GetLength(0);
+    }
+
+    public static int Width(this Game.Sudoku sudoku)
+    {
+        return sudoku.Cells.GetLength(1);
+    }
+}
