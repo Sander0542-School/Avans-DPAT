4eaad17cf4e1936a4681d5aa71baf5c640719c97
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-14 11:31:46 +0200

    Create ICell[,].FirstEmptyCell extension method

---
+++ src/Avans.DPAT.Sudoku.Game/Extensions/ArrayExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Avans.DPAT.Sudoku.Game.Grid.Common;
+
+namespace Avans.DPAT.Sudoku.Game.Extensions;
+
+public static class ArrayExtensions
+{
+    public static ICell? FirstEmptyCell(this ICell[,] cells)
+    {
+        return cells.OfType<ICell>().FirstOrDefault(cell => cell.Value.HasValue == false);
+    }
+}
